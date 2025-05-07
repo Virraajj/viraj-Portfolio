123f63ba2a97f433fc04015e271d62d9f6ae19e6
Viraj Chudasama <virajchudasama@Virajs-Laptop.local>
2025-05-07 19:19:43 +0530

    Initial commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,10 +15,10 @@ const navLinks = [
     name: "Skills",
     link: "#skills",
   },
-  {
-    name: "Testimonials",
-    link: "#testimonials",
-  },
+  // {
+  //   name: "Testimonials",
+  //   link: "#testimonials",
+  // },
 ];
 
 const words = [
